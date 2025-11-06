ffea955d2aaf07823316304fd76bd07ef3661452
thenightking-94 <c.suvam@gmail.com>
2025-11-07 00:50:23 +0530

    corrected async logics as per new ES9

--- PromiseAllPolyfill.js
+++ PromiseAllPolyfill.js
@@ -1,43 +1,46 @@
-let p1 = Promise.resolve("hello");
-let p2 = new Promise((resolve, reject) => {
+const p1 = Promise.resolve("hello");
+const p2 = new Promise((resolve, reject) => {
   setTimeout(resolve, 100, 'world');
 });
-
-
+const p3 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("polyfill-1")
+  }, 2000);
+});
+const p4 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("polyfill-2")
+  }, 2000);
+});
+const p5 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("polyfill-3")
+  }, 2000);
+})
+const e1 = Promise.reject("test-rej");
 Promise.myAll = (promises) =>
 {
-  //creating another promise object for the polyfill main resolving to the array of all the responses
- return new Promise((resol,rejec)=>{
-    let totalPromises = promises.length;
-    let responses=[]
-    let resCount=0,rejCount=0; 
-   
-    promises.forEach(function countResolves (eachPromise, index){
-      
-   
-    eachPromise.then(function(res)
-         {
-          resCount++;
-          console.log(index,eachPromise)
-          responses=[...responses,res];
-          if(index===0)
-              console.log("first one resolved")
-          if( resCount===totalPromises){
-              console.log("All of the promise objects resolved")
-              resol(responses)
-          }
-         },
-     function(rej)
-        { rejCount++;}
-                   )
- 
-  })
-    
+ return new Promise(async (resol, rejec) => {
+  // async functions always returns a promise implicitly whether or not explicitly wrapped or not
+    let responses = [];
+    try {
+      for await (const promise of promises) {
+          const res = await promise;
+          responses = [...responses, res];
+        }
+      if(responses.length === promises.length)
+        return resol(responses);
+    }
+    catch(e) {
+      return rejec(e);
+    }
  })
-                     
 }
-
-Promise.myAll([p1,p2]).then(
-  function(data){console.log("My all resolved",data);},
-  function(rej){console.log("My all rejected",rej);}
+Promise.myAll([p1,p2,p4,p5,p3]).then(
+  function(data){console.log("Resolved", data)}
 )
+Promise.myAll([p1,p2,e1,p4,p5,p3]).then(
+  function(data){console.log("My all resolved", data);},
+  function(rej){console.log("My all rejected", rej);}
+)
+//best run on chrome v8 for better experience (error handling in edgy on node)
