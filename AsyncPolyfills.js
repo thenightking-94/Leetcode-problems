8932155b9aac959b846b99ab4c775d89bcbba9fc
thenightking-94 <67338546+thenightking-94@users.noreply.github.com>
2025-11-07 19:59:39 +0530

    Refactor Promise polyfills for better performance
    Refactor recursiveIterator and Promise.AllSettled to improve clarity and functionality.

--- AsyncPolyfills.js
+++ AsyncPolyfills.js
@@ -1,5 +1,6 @@
 // Polyfills
-
+// on large scale production always use promise.allsettled or generators
+//recursively to generate data packets at intervals instead loop based sols
 const p = Promise.resolve('test');
 const q = Promise.resolve('test-1');
 const er = Promise.reject('err');
@@ -7,7 +8,7 @@ const er2 = Promise.reject('err2');
 const t = new Promise((res) =>
   setTimeout(() => {
     res('time');
-  }, 2000)
+  }, 3000)
 );
 const p1 = Promise.resolve('recursiveIterator');
 
@@ -28,21 +29,17 @@ Promise.All = (promises) => {
 
 Promise.AllSettled = (promises) => {
   return new Promise(async (r) => {
-    return r(await recursiveIterator(promises, 0, [], promises));
+    return r(await recursiveIterator(promises, [], promises));
   });
 };
 
-const recursiveIterator = async (pList, index, masterList, orgList) => {
-  console.log(pList, index, masterList, orgList);
+const recursiveIterator = async (pList, masterList, orgList) => {
+  console.log(pList, masterList, orgList);
   return new Promise(async (resolve) => {
-    let acc = [],
-      count = index;
+    let acc = [];
     try {
       for await (const res of pList) {
-        const iteratedAcc = [...acc];
         acc = [...acc, res];
-        //only increment count for driving iteration if its a valid value, and not thrown on catch block
-        if (acc.length > iteratedAcc.length) count++;
         if ([...masterList, ...acc].length === orgList.length)
           return resolve([...masterList, ...acc]);
       }
@@ -50,8 +47,8 @@ const recursiveIterator = async (pList, index, masterList, orgList) => {
       acc = [...acc, er];
       return resolve(
         recursiveIterator(
+          // discard the counts that have been processed
           orgList.slice([...masterList, ...acc].length),
-          count + 1,
           [...masterList, ...acc],
           orgList
         )
